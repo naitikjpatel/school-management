263ad9d866e91362c85e7184b8589b177da00eea
naitikjpatel <njpatel20031011@gmail.com>
2025-05-12 16:21:31 +0530

    .

---
+++ src/main/java/com/school/configuration/WebConfig.java
@@ -0,0 +1,16 @@
+package com.school.configuration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // Allow frontend origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
